3d8c41f5fc01fb283132ccd5538c5480df2cc0e0
carlosEduardoRincon <carloscaca147@gmail.com>
2026-05-27 20:39:05 -0300

    feat: alteração de estrutura e resolução do exercicio de two pointer

---
+++ java-estrutura-de-dados/src/main/java/rincon/com/br/TwoPointer_ReverseWordInAString.java
@@ -0,0 +1,44 @@
+package rincon.com.br;
+
+import java.util.Arrays;
+
+public class TwoPointer_ReverseWordInAString {
+
+    public static void main(String[] args) {
+        System.out.println(reverseWords("Let's take LeetCode contest"));
+    }
+
+    public static String reverseWords(String s) {
+        int leftPointer = 0;
+        int rightPointer = 0;
+
+        var stringArray = s.toCharArray();
+        while (rightPointer < s.length()) {
+            if (stringArray[rightPointer] != ' ') {
+                rightPointer += 1;
+            } else {
+                var initialRightPointer = rightPointer;
+                for (int i = leftPointer; i < rightPointer-1; i++) {
+                    var aux = stringArray[i];
+                    stringArray[i] = stringArray[rightPointer-1];
+                    stringArray[rightPointer-1] = aux;
+                    rightPointer--;
+                }
+
+                rightPointer = initialRightPointer + 1;
+                leftPointer = rightPointer;
+            }
+        }
+
+        for (int i = leftPointer; i < rightPointer-1; i++) {
+            var aux = stringArray[i];
+            stringArray[i] = stringArray[rightPointer-1];
+            stringArray[rightPointer-1] = aux;
+            rightPointer--;
+        }
+
+        StringBuilder response = new StringBuilder();
+        return String.valueOf(response.append(stringArray));
+    }
+
+}
---
+++ java-estrutura-de-dados/teorico/tipos-problemas/arrays/TwoPointer.md
@@ -0,0 +1,9 @@
+### Two pointer
+
+Técnica para resolução de problemas utilizando strings e arrays;
+Ex.:
+  - Inverter palavras numa string;
+
+Ela consiste em inicialmente iniciar dois ponteiros no array: um no início e outro no fim,
+para não alocar espaço adicional na memória;
+
