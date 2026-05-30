78b972bac1f1bf76dd60289e38e2c05c20b941a6
carlosEduardoRincon <carloscaca147@gmail.com>
2026-05-30 18:24:51 -0300

    feat: adição de resolução de exercicio two pointer

--- java-estrutura-de-dados/src/main/java/rincon/com/br/TwoPointer_ReverseWordInAString.java
+++ java-estrutura-de-dados/src/main/java/rincon/com/br/TwoPointer_ReverseWordInAString.java
@@ -7,6 +7,7 @@ public class TwoPointer_ReverseWordInAString {
     public static void main(String[] args) {
         System.out.println(reverseWords("Let's take LeetCode contest"));
         System.out.println(reverseWords2("Let's take LeetCode contest"));
+        System.out.println(reverseStr3("abcdefg", 8));
     }
 
     public static String reverseWords(String s) {
@@ -54,4 +55,31 @@ public class TwoPointer_ReverseWordInAString {
         return result.toString();
     }
 
+
+    public String reverseStr(String s, int k) {
+        char[] str = s.toCharArray();
+        int n = s.length();
+
+        for (int i = 0; i < n; i += 2 * k) {
+            if (i + k - 1 <= n - 1) {
+                reverseK(i, i + k - 1, str);
+            } else {
+                reverseK(i, n - 1, str);
+
+            }
+        }
+
+        String revStr = new String(str);
+        return revStr;
+    }
+
+    public void reverseK(int i, int j, char[] str) {
+        while (i < j) {
+            char temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+            i++;
+            j--;
+        }
+    }
 }
