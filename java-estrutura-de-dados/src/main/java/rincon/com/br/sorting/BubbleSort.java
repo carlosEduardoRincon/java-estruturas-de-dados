d02e8aeafa1f9116ef08db57f9c004f9864be52e
carlosEduardoRincon <carloscaca147@gmail.com>
2026-07-30 08:09:25 -0300

    feat: BubbleSort improve

--- java-estrutura-de-dados/src/main/java/rincon/com/br/sorting/BubbleSort.java
+++ java-estrutura-de-dados/src/main/java/rincon/com/br/sorting/BubbleSort.java
@@ -37,11 +37,16 @@ public class BubbleSort {
         int length = arr.length;
 
         for (int i = 0; i < length; i++) {
+            var isSorted = true;
             for (int j = 0; j < length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
+                    isSorted = false;
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                }
+                if (isSorted) {
+                    return arr;
                 }
             }
         }
