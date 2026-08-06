12d28136623e09963fc392ccaa0dd5d54139ec03
carlosEduardoRincon <carloscaca147@gmail.com>
2026-08-06 07:11:32 -0300

    feat: Merge sort

--- java-estrutura-de-dados/src/main/java/rincon/com/br/dataStructure/linkedList/ListNode.java
+++ java-estrutura-de-dados/src/main/java/rincon/com/br/dataStructure/linkedList/ListNode.java
@@ -3,9 +3,12 @@ package rincon.com.br.dataStructure.linkedList;
 public class ListNode {
       public int val;
       public ListNode next;
-      ListNode() {}
+      public ListNode() {
+            this.val = 0;
+            this.next = null;
+      }
       public ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
       public int getVal() {
             return this.val;
--- java-estrutura-de-dados/src/main/java/rincon/com/br/sorting/MergeSort.java
+++ java-estrutura-de-dados/src/main/java/rincon/com/br/sorting/MergeSort.java
@@ -1,5 +1,7 @@
 package rincon.com.br.sorting;
 
+import rincon.com.br.dataStructure.linkedList.ListNode;
+
 import java.util.Arrays;
 
 /*
@@ -17,26 +19,96 @@ import java.util.Arrays;
 public class MergeSort {
 
     public static void main(String[] args) {
-        System.out.println("---------------------------------");
-        System.out.println(Arrays.toString(sort(new int[]{5}))); // [5]
-        System.out.println("---------------------------------");
-        System.out.println(Arrays.toString(sort(new int[]{}))); // []
-        System.out.println("---------------------------------");
-        System.out.println(Arrays.toString(sort(new int[]{5, 4, 3, 2, 1}))); // [1, 2, 3, 4, 5]
-        System.out.println("---------------------------------");
-        System.out.println(Arrays.toString(sort(new int[]{1, 2, 3, 4, 5}))); // [1, 2, 3, 4, 5]
-        System.out.println("---------------------------------");
-        System.out.println(Arrays.toString(sort(new int[]{3, 3, 2, 1, 2}))); // [1, 2, 2, 3, 3]
-        System.out.println("---------------------------------");
-        System.out.println(Arrays.toString(sort(new int[]{-5, 10, 0, -1}))); // [-5, -1, 0, 10]
-        System.out.println("---------------------------------");
+
+        // Lista: 4 -> 2 -> 1 -> 3 -> 5
+        ListNode head = new ListNode(
+                4,
+                new ListNode(
+                        2,
+                        new ListNode(
+                                1,
+                                new ListNode(
+                                        3,
+                                        new ListNode(5)
+                                )
+                        )
+                )
+        );
+
+        System.out.println("Lista original:");
+        printList(head);
+
+        head = sort(head);
+
+        System.out.println("\nLista ordenada:");
+        printList(head);
     }
 
-    public static int[] sort(int[] arr) {
-        return mergeSort(arr, 0, arr.length - 1);
+    private static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+
+            if (head.next != null) {
+                System.out.print(" -> ");
+            }
+
+            head = head.next;
+        }
+
+        System.out.println();
     }
 
-    private static int[] mergeSort(int[] arr, int i, int i1) {
-        return new int[]{};
+    public static ListNode sort(ListNode head) {
+        return mergeSort(head);
     }
+
+    private static ListNode mergeSort(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        var middle = findMiddle(head);
+        var afterMiddle = middle.next;
+        middle.next = null;
+        var left = mergeSort(head);
+        var right = mergeSort(afterMiddle);
+
+        return merge(left, right);
+    }
+
+    private static ListNode findMiddle(ListNode head){
+        var slow = head;
+        var fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    private static ListNode merge(ListNode l1, ListNode l2) {
+        var head = new ListNode();
+        var tail = head;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        if (l1 != null) {
+            tail.next = l1;
+        } else if (l2 != null) {
+            tail.next = l2;
+        }
+
+        return head.next;
+    }
+
 }
